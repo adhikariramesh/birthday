6512ec1a6d5a44cf030b52d3b15c7c811b891ef1
Ramesh Prasad Adhikari <107765170+adhikariramesh@users.noreply.github.com>
2023-05-26 15:19:03 +0545

    complet

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,43 @@
-import React from 'react'
+"use client";
+import { useState } from 'react';
+import Image from 'next/image';
+import UserData from '@/utils/UserData';
+
 
 const page = () => {
+  const [user, setUser] = useState(UserData);
+  const [count, setCount] = useState(5);
+  const handleClear = ()=>{
+    const container = document.querySelectorAll(".container");
+    container.forEach((cr,index)=>{
+      container[index].style.display = "none";
+      setCount(0);
+    });
+  }
   return (
-    <div>
-      this si main
-    </div>
+    <main>
+      <h1 className="title"> {count} Birthdays Today</h1>
+      <div className="main_container">
+        {
+          user.map((curElmnt) => {
+            return (
+                <div className="container" key={curElmnt.id}>
+                  <div className="imgBody">
+                    <Image src={curElmnt.imgUrl} alt='userImage' width={200} height={200} />
+                  </div>
+                  <div className="content">
+                    <h2 className="name">{curElmnt.name}</h2>
+                    <p className="age">{curElmnt.age} years</p>
+                  </div>
+                </div>
+            )
+          })
+        }
+      </div>
+      <button className="btn"
+      onClick={handleClear}
+      >clear all</button>
+    </main>
   )
 }
 
---
+++ src/utils/UserData.js
@@ -0,0 +1,33 @@
+const UserData = [
+    {
+        id:1,
+        name:"menika Yadav",
+        age:22,
+        imgUrl:"/person-1.jpeg"
+    },
+    {
+        id:2,
+        name:"Hester Hogan",
+        age:27,
+        imgUrl:"/person-2.jpeg"
+    },
+    {
+        id:3,
+        name:"men in black",
+        age:25,
+        imgUrl:"/person-3.jpeg"
+    },
+    {
+        id:4,
+        name:"done bosko",
+        age:27,
+        imgUrl:"/person-4.jpeg"
+    },
+    {
+        id:5,
+        name:"lali",
+        age:29,
+        imgUrl:"/person-5.jpeg"
+    },
+];
+export default UserData;
